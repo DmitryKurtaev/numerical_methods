e5951b8cc35679306cfbc94ee74f1508222c4ac2
DmitryKurtaev <dmitry.kurtaev@gmail.com>
2016-03-08 19:59:07 +0300

    Edited results printing

--- lab/mpi_with_optim_step_lab.cc
+++ lab/mpi_with_optim_step_lab.cc
@@ -67,9 +67,9 @@ int main(int argc, char** argv) {
     const int m = n_intervals_by_y;
     const double h = (kRightBorder - kLeftBorder) / n;
     const double k = (kTopBorder - kBottomBorder) / m;
-    for (int j = 1; j < m; ++j) {
+    for (int j = 0; j <= m; ++j) {
       const double y = j * k;
-      for (int i = 1; i < n; ++i) {
+      for (int i = 0; i <= n; ++i) {
         robust_values.push_back(exp(pow(sin(M_PI * i * h * y), 2)));
       }
     }
@@ -117,7 +117,7 @@ void Solve(int n_intervals_by_x, int n_intervals_by_y,
   }
 
   double accuracy = DBL_MAX;
-  for (int iter = 0; iter < n_iters && (robust_values.size() != dim ||
+  for (int iter = 0; iter < n_iters && (robust_values.empty() ||
                                         accuracy >= eps); ++iter) {
     double* buf = new double[dim];
     memcpy(buf, x, sizeof(double) * dim);
@@ -204,10 +204,15 @@ void Solve(int n_intervals_by_x, int n_intervals_by_y,
     buf[idx] += step * (b[idx] - term);
 
     // Compute accuracy.
-    if (robust_values.size() == dim) {
+    if (!robust_values.empty()) {
       accuracy = 0;
-      for (int i = 0; i < dim; ++i) {
-        accuracy = std::max(accuracy, fabs(x[i] - robust_values[i]));
+      int idx = 0;
+      for (int j = 1; j < m; ++j) {
+        for (int i = 1; i < n; ++i) {
+          accuracy = std::max(accuracy,
+                              fabs(x[idx] - robust_values[j * (n + 1) + i]));
+          ++idx;
+        }
       }
     }
 
@@ -292,38 +297,58 @@ void Print(int n_intervals_by_x, int n_intervals_by_y,
   
   // Extract accuracy.
   double max_diff = 0;
-  int argmax = 0;
-  for (int i = 0; i < (n - 1) * (m - 1); ++i) {
-    if (fabs(robust_values[i] - result[i]) > max_diff) {
-      max_diff = fabs(robust_values[i] - result[i]);
-      argmax = i;
+  int argmax_i = 0;
+  int argmax_j = 0;
+  int idx = 0;
+  for (int j = 1; j < m; ++j) {
+    for (int i = 1; i < n; ++i) {
+      double diff = fabs(result[idx] - robust_values[j * (n + 1) + i]);
+      ++idx;
+      if (diff > max_diff) {
+        max_diff = diff;
+        argmax_i = i;
+        argmax_j = j;
+      }
     }
   }
 
-  std::vector<std::vector<std::string> > data(m);
-  for (int i = 0; i < m; ++i) {
-    data[i].resize(n);
+  // Robust values.
+  std::vector<std::vector<std::string> > data(m + 2);
+  for (int i = 0; i < m + 2; ++i) {
+    data[i].resize(n + 2);
   }
   data[0][0] = "Robust values";
-  for (int i = 0; i < n - 1; ++i) {
+  for (int i = 0; i <= n; ++i) {
     std::ostringstream ss;
-    ss << (i + 1) * h;
+    ss << i * h;
     data[0][i + 1] = ss.str();
 
-    for (int j = 0; j < m - 1; ++j) {
+    for (int j = 0; j <= m; ++j) {
       std::ostringstream ss;
-      ss << robust_values[(m - 2 - j) * (n - 1) + i];
+      ss << robust_values[(m - j) * (n + 1) + i];
       data[j + 1][i + 1] = ss.str();
     }
   }
-  for (int j = 0; j < m - 1; ++j) {
+  for (int j = 0; j <= m; ++j) {
     std::ostringstream ss;
-    ss << (m - 1 - j) * k;
+    ss << (m - j) * k;
     data[j + 1][0] = ss.str();
   }
   TablePrinter::Print(data);
 
+  // Results.
+  std::cout << "max|V-" << (task == TEST ? "U" : "V2") << "| = "
+            << max_diff << " (at x[" << argmax_i << "]=" << h * argmax_i
+            << ", y[" << argmax_j << "]=" << k * argmax_j << ")"
+            << std::endl;
+
   data[0][0] = "Results";
+  data.erase(data.end());
+  data.erase(data.begin() + 1);
+  for (int i = 0; i < data.size(); ++i) {
+    data[i].erase(data[i].end());
+    data[i].erase(data[i].begin() + 1);
+  }
   for (int i = 0; i < n - 1; ++i) {
     for (int j = 0; j < m - 1; ++j) {
       std::ostringstream ss;
@@ -333,8 +358,15 @@ void Print(int n_intervals_by_x, int n_intervals_by_y,
   }
   TablePrinter::Print(data);
 
-
-    // std::cout << "max|V-" << (task == TEST ? "U" : "V2") << "| = "
-    //         << max_diff << " (at x[" << argmax << "]=" h * argmax << ")"
-    //         << std::endl;
+  // Accuracy.
+  data[0][0] = "Differences";
+  for (int i = 0; i < n - 1; ++i) {
+    for (int j = 0; j < m - 1; ++j) {
+      std::ostringstream ss;
+      ss << fabs(result[(m - 2 - j) * (n - 1) + i] -
+                 robust_values[(m - j - 1) * (n + 1) + i + 1]);
+      data[j + 1][i + 1] = ss.str();
+    }
+  }
+  TablePrinter::Print(data);
 }
